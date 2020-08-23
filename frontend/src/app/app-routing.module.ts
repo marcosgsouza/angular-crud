41734bb45c2b4c7d08717d2437325d7b4b83d2bf
Marcos Vinicius <marcosvinicius_gsouza@outlook.com>
2020-08-23 17:54:43 -0300

    Criando rota para criação de novo produto

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './views/home/home.component';
 import { ProductCrudComponent } from './views/product-crud/product-crud.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 
 const routes: Routes = [{
   path: "",
@@ -10,6 +11,9 @@ const routes: Routes = [{
 }, {
   path: "products",
   component: ProductCrudComponent
+}, {
+  path: "products/create",
+  component: ProductCreateComponent
 }];
 
 @NgModule({
